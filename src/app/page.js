27782f4cbb3da21d66b8dc5e679076e9fa646e47
minddymind <chananchidapratoom6@gmail.com>
2024-12-07 16:16:45 +0700

    add link to login and sign up

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ export default function Home() {
       font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <h1 className="animate-slidein opacity-0 [--slidein-delay:300ms] sm:text-3xl md:text-3xl lg:text-6xl 
+          <h1 className="sm:text-3xl md:text-3xl lg:text-6xl 
           relative w-[max-content] before:absolute before:inset-0 before:animate-typewriter before:bg-white after:absolute 
           after:inset-0 after:w-[0.125em] after:animate-caret after:bg-black">
             <b>Welcome to Taskly – Simplify Teamwork!</b>
           </h1>
         </div>
-        <h4 className="animate-slidein opacity-0 [--slidein-delay:300ms]">
+        <h4 className="animate-slidein opacity-0 [--slidein-delay:300ms] text-lg">
           Designed for small teams, Taskly helps you stay organized, focused,
           and connected.
         </h4>
@@ -30,10 +30,10 @@ export default function Home() {
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <button className="bg-transparent hover:bg-blue-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border 
             border-blue-500 hover:border-transparent rounded-full">
-              Sign Up
+              <a href="/signup">Sign Up</a>
             </button>
             <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-full">
-              Log in
+              <a href="/login">Log in</a>
             </button>
           </div>
         </div>
